2d33239f0bf0a4e7ea545c1331d40151809d10b5
Jeanjean <adrien.jeanjean@epsi.fr>
2021-03-19 18:02:08 +0100

    :tada: script

---
+++ installTmuxNvim.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+apt install neovim
+apt install wget
+apt install tmux
+apt install git
+mkdir -p ~/.vim/autoload/
+cd ~/.vim/autoload/
+wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir ~/Documents/scripts
+cd ~/Documents/scripts
+git clone https://github.com/Amtilaw/VimAndTmuxScript.git
+cd VimAndTmuxScript
+cp init.vim ~/.config/nvim/
+cp .tmux.conf ~/
